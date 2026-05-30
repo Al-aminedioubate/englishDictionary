be127cbf064c87a32f381abb077a6041f85ee9fc
Lamine DIOUBATE <domanilamine96@gmail.com>
2026-05-29 22:16:00 -0400

    ajustemetn du code

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 //Declaration et initialisation des variables
 let input = document.getElementById("txtinput");
 let txtMeaning = document.getElementById("showWord");
-
-//Debut du traitement continue
