4f83ca6951265b1eaab295fd88435ae7efb6b5b4
Lamine DIOUBATE <domanilamine96@gmail.com>
2026-05-30 22:40:05 -0400

    condition done

--- index.js
+++ index.js
@@ -7,5 +7,9 @@ function findMeaning() {}
 
 //detectons la touche entrée
 input.addEventListener("keydown", (event) => {
-    if(event.key === 'Enter')
+	if (event.key === "Enter") {
+		event.preventDefault();
+
+		console.log("la touche entree a ete pressee!");
+	}
 });
