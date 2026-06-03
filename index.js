7e5e0d2170e41deade5d7cc128381a63a463dc0e
Lamine DIOUBATE <domanilamine96@gmail.com>
2026-06-02 21:03:17 -0400

    mise a jour du coded

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //Declaration et initialisation des variables
 let input = document.getElementById("txtinput");
-let txtMeaning = document.getElementById("showWord");
+let txtInfo = document.getElementById("infoText");
 
 //Fonction du traitement des mots
 async function getDefinition(word) {
