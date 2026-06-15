d563c21db9310fa8ef2650fbaa4ec3b90a746f42
Lamine DIOUBATE <domanilamine96@gmail.com>
2026-06-14 22:00:49 -0400

    correction d'erreur

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ async function getDefinition(word) {
 			meaningContainerEl.style.display = "block";
 			titleWord.innerText = data[0].word;
 			meaningWord.textContent = data[0].meanings[0].definitions[0].definition;
-			audioEl.src = data[0].phonetic[0].audio;
+			audioEl.src = data[0].phonetics[0].audio;
 		}
 	} catch (error) {
 		txtInfo.textContent = `An error happen ${error}`;
